0552edffa7b5e33cdfcc402cecbe67d7ecd844c5
tanmoysaha <tanmoyhere07@gmail.com>
2026-03-29 16:16:42 +0530

    Delete showcase directory

--- showcase/data.js
+++
@@ -1,8 +0,0 @@
-// data.js - Holds the configuration for your showcased websites
-const SHOWCASE_DATA = [];
-
-// If we're in a common js environment (like Node) we'll export it, 
-// but for standard browsers we just let it sit in the global window object.
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = SHOWCASE_DATA;
-}
